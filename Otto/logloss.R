39ec1d049b2c432ab3b3707390e1261b51df5c9c
willy2721 <b02705006@ntu.edu.tw>
2017-06-08 00:19:14 -0700

    refactor

--- Otto/logloss.R
+++
@@ -1,9 +0,0 @@
-# Multiclass logloss function
-logloss <- function(pred, tar){
-  max <- 1 - 10 ^ (-15)
-  min <- 10 ^ (-15)
-  pred[pred > max] = max
-  pred[pred < min] = min
-  rownum <- nrow(tar)
-  return(-sum(tar * log(pred)) / rownum)
-}
