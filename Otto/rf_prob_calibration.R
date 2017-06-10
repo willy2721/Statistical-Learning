d32574d30130c3ecca51954e053b5d31487fdaeb
Willy <b02705006@ntu.edu.tw>
2017-06-10 19:36:50 +0800

    add function for bagging probability calibration

--- Otto/rf_prob_calibration.R
+++ Otto/rf_prob_calibration.R
@@ -70,3 +70,81 @@ rf_prob_calibration <- function(subtrain, caliset, cali_answer, subtrain_rf, rat
   return(list(class_probs = class_probs, cali_predict = cali_predict, cali_model = cali_model, platt_model = platt_model))
   
 }
+
+# Input : Training set, testing set, number of bagging(1 if no bag)
+# Output : Predicted probabilities, isoReg calibrated probabilities and Platt calibrated probabilities for each class
+rf_cali_cv_bagged <- function(train, test, nbag, ncv){
+  train_meta_list <- list()
+  test_predict_bagged <- matrix(0, nrow = 144368, ncol = 9)
+  
+  # Bag the model 
+  for(n in 1 : nbag){
+    # Cross-validate calibration
+    folds <- createFolds(train$target, k = nbag, list = FALSE)
+    train$fold <- folds
+    
+    # Store meta feature for train and predict
+    
+    train_meta <- data.frame()
+    test_predict_combined <- matrix(0, nrow = nrow(test), ncol = ncol(test))
+    
+    # K-fold cross-validation
+    for(k in 1 : ncv){
+      
+      # Seperate the training, calibration folds    
+      calibrate_i <- which(train$fold == k)
+      subtrain <- train[-calibrate_i, ]
+      caliset <- train[calibrate_i, ]
+      cali_answer <- train_answer[calibrate_i,]
+      
+      # Remember to remove folds when training !
+      # Train random forest on subtrain
+      subtrain_rf <- randomForest(subtrain[,c(-1,-95,-96)], as.factor(subtrain$target), ntree = 800, mtry = 20, nodesize = 1)
+      # Predict for calibrate set 
+      cali_predict <- predict(subtrain_rf, caliset[,c(-1,-95,-96)], type="prob")
+      # Predict for test set 
+      test_predict <- predict(subtrain_rf, test[,-1], type="prob")
+      
+      # Loop through each class
+      for(i in 1:9){
+        # Store predicted class probability and correct class
+        pred_is_class <- cali_predict[,i]
+        ans_is_class <- factor(cali_answer[,i + 1])
+        ans_is_class <- factor(ans_is_class,levels(ans_is_class)[c(2,1)])
+        
+        # Calibrate the probabilities for test set
+        calibration <- calibrate(ans_is_class, pred_is_class, class1 = 1, method = "isoReg", weight=NULL, noBins=10, assumeProbabilities=TRUE)
+        test_predict[,i] <- applyCalibration(test_predict[,i], calibration)
+        cali_predict[,i] <- applyCalibration(cali_predict[,i], calibration)
+      } 
+      
+      # Add prediction to test_predict_combined and train_meta
+      test_predict_combined <- test_predict_combined + test_predict
+      train_meta <- rbind(train_meta, cali_predict)
+      
+    }
+    
+    # Get the average of three test_predictions (also meta feature for test)
+    test_predict_combined <- test_predict_combined / ncv
+    
+    # Order meta feature dataframe by row name
+    train_meta[order(as.numeric(row.names(train_meta))),]
+    train_meta_list[[n]] <- train_meta
+    test_predict_bagged <- test_predict_bagged + test_predict_combined
+    
+  }
+  # Get average
+  train_meta_bagged <- train_meta_list[[1]]
+  if(ncv > 1){
+    for(cv in 2 : ncv){
+      train_meta_bagged <- train_meta_bagged + train_meta_list[[cv]]
+    }
+    train_meta_bagged <- train_meta_bagged / ncv
+  }
+  test_predict_bagged <- test_predict_bagged / ncv
+  
+  return(list(train_meta_bagged = train_meta_bagged, test_predict_bagged <- test_predict_bagged))
+  
+}
+
+
